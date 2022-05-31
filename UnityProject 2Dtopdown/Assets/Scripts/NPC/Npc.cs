eecceac3958ae544ffefcb39964c3a3dc6fcc200
Leonardo Martins <94061150+leomartinss@users.noreply.github.com>
2022-05-30 22:17:38 -0300

    update

--- UnityProject 2Dtopdown/Assets/Scripts/NPC/Npc.cs
+++ UnityProject 2Dtopdown/Assets/Scripts/NPC/Npc.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Npc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //variáveis 
+    public float speed;
 
-    // Update is called once per frame
+    private int index;
+
+    public List<Transform> paths = new List<Transform>();
+    
     void Update()
     {
-        
+        //movendo o npc em direção ao path 
+        transform.position = Vector2.MoveTowards(transform.position, paths[index].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, paths[index].position) < 0.1f) 
+        {
+            if(index < paths.Count - 1) 
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+
+        Vector2 direction = paths[index]. position - transform.position;
+
+        if(direction.x > 0) 
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if(direction.x < 0) 
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
     }
 }
