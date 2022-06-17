cfb8358e8a9e87d90d2d052ac4856c3c6479f923
Leonardo Martins <94061150+leomartinss@users.noreply.github.com>
2022-06-16 21:21:22 -0300

    update

--- UnityProject 2Dtopdown/Assets/Scripts/Crafting/Tree/Tree.cs
+++ UnityProject 2Dtopdown/Assets/Scripts/Crafting/Tree/Tree.cs
@@ -8,6 +8,9 @@ public class Tree : MonoBehaviour
     [SerializeField] private float treeHealth;
     [SerializeField] private Animator anim;
 
+    [SerializeField] private GameObject woodPrefab; // prefab of wood
+    [SerializeField] private int totalWood; // total wood in the tree
+
     public void OnHit()
     {
         treeHealth --;
@@ -16,6 +19,11 @@ public class Tree : MonoBehaviour
 
         if (treeHealth <= 0)
         {
+            for(int i = 0; i < totalWood; i++)
+            {
+                //cria o prefab de madeira
+                Instantiate(woodPrefab, transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, -1f), 0f), transform.rotation);
+            }
             anim.SetTrigger ("cut");
         }
     }
